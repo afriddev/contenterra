38cfdc7fe20af46f22210adb6ea3e449e97b696c
afriddev <afridayan01@gmail.com>
2024-12-08 22:29:41 +0000

    modified ui

--- src/ui/Card.tsx
+++ src/ui/Card.tsx
@@ -14,38 +14,38 @@ function Card({ score, selftextHtml, title, url }: CardInterface) {
 
   const decodedHtml = decodedHTMLToString();
   return (
-    <div className="w-full p-2 pt-2 rounded-md lg:min-h-[20vh] shadow-md border border-black lg:hover:shadow-2xl lg:hover:scale-101">
+    <div className="w-full pb-2  rounded-md lg:min-h-[20vh] shadow-md border border-black lg:hover:shadow-2xl lg:hover:scale-101">
       <div className="flex flex-col w-full">
-        <label className="flex justify-center">
-          <span className="w-fit font-semibold border-b-2  border-black text-center">
+        <label className="flex justify-center rounded-t-md py-5 bg-yellow-400">
+          <span className="w-fit font-semibold  text-center px-2">
             {title}
           </span>
         </label>
-        <div className="flex flex-col gap-3 mt-5">
-          <label className="font-semibold">
+        <div className="flex flex-col gap-3 mt-5 px-2">
+          <label className="font-semibold text-lg">
             Score: <span className="font-normal">{score}</span>
           </label>
-          <a
-            href={url}
-            className="w-full text-nowrap cursor-pointer font-semibold"
+
+          <button className="border border-black bg-black w-fit h-fit px-4 rounded-md py-1 flex items-center justify-center  text-white " title={url}
+            onClick={() => {
+              if (url) window.open(url);
+            }}
           >
-            Url:{" "}
-            <span className="text-blue  text-xs text-wrap break-words">
-              {url}
-            </span>
-          </a>
+            Read More
+          </button>
+          
         </div>
       </div>
 
-      {
-        decodedHtml && <div className="mt-4" >
-        <label className="font-semibold"> Self HTML text :</label>
-        <div
-          className="html-content break-words break-all mt-2 overflow-auto max-w-[95vw] lg:max-w-[30vw]  max-h-[20vh]"
-          dangerouslySetInnerHTML={{ __html: decodedHtml ?? "" }}
-        />
-      </div>
-      }
+      {decodedHtml && (
+        <div className="mt-6 px-2 relative  border border-black  mx-2 lg:mx-4 rounded-md  flex flex-col ">
+          <label className="font-semibold absolute bg-white -mt-3 rounded-sm lg:-mt-3  ml-2  lg:ml-1 px-4" > Self HTML text</label>
+          <div
+            className="html-content  rounded-lg py-1 px-1  break-words break-all mt-4 overflow-auto max-w-[95vw] lg:max-w-[30vw]  max-h-[20vh]"
+            dangerouslySetInnerHTML={{ __html: decodedHtml ?? "" }}
+          />
+        </div>
+      )}
     </div>
   );
 }
